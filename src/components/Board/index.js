37de0102c71a413c9f81c99017d33bf19153dddf
Douglas Lopes <douglasdmlopes@gmail.com>
2019-12-22 11:31:04 -0300

    Start

---
+++ src/components/Board/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { loadLists } from '../../services/api';
+
+import List from '../List';
+
+import { Container } from './styles';
+
+const lists = loadLists();
+
+export default function Board() {
+  return (
+    <Container>
+      {lists.map(list => <List key={list.title} data={list} />)}
+    </Container>
+  );
+}
---
+++ src/components/Card/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useDrag} from 'react-dnd';
+
+import { Container, Label } from './styles';
+
+export default function Card({data}) {
+
+  const [{isDragging}, dragRef] = useDrag({
+    item: {type: 'CARD'},
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <Container ref={dragRef} isDragging={isDragging}>
+      <header>
+        {data.labels.map(label => <Label key={label} color={label}/>)}
+      </header>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt=""/>}
+    </Container>
+  );
+}
